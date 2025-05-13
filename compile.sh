37f7b00cb7d816f536109cf5b09e94604bad3a00
Jony <102042390+jonyblan@users.noreply.github.com>
2025-05-13 01:25:26 -0300

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+# Compila el proyecto a traves del container de Docker
+docker start tpe_arqui_1q2024
+docker exec -ti tpe_arqui_1q2024 make clean -C/root/Toolchain
+docker exec -ti tpe_arqui_1q2024 make clean -C/root
+docker exec -ti tpe_arqui_1q2024 make all -C/root/Toolchain
+docker exec -ti tpe_arqui_1q2024 make all -C/root
+docker stop tpe_arqui_1q2024
