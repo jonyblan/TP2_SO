e123f2f9fb9174d72ce6bf721f25b75b26716bb8
agRamirez2003 <aramirezdonoso@itba.edu.ar>
2025-06-09 11:44:12 -0300

    gitignore y cambio al compile.sh por algo mio de permisos

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ docker exec -ti tpe_arqui_1q2024 make clean -C/root/Toolchain
 docker exec -ti tpe_arqui_1q2024 make clean -C/root
 docker exec -ti tpe_arqui_1q2024 make all -C/root/Toolchain
 docker exec -ti tpe_arqui_1q2024 make all -C/root
+docker exec -ti tpe_arqui_1q2024 chown -R 1000:1000 /root
 docker stop tpe_arqui_1q2024
