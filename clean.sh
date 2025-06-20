1dc395a15ea5eb53389f562010f122533873c62a
agRamirez2003 <aramirezdonoso@itba.edu.ar>
2025-06-20 16:03:07 -0300

    arreglando los sh para todos

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 # Limpia el proyecto a traves del container de Docker
-docker start tpSO3
-docker exec -ti tpSO3 make clean -C/root/Toolchain
-docker exec -ti tpSO3 make clean -C/root
-docker stop tpSO3
+docker start tpe_arqui_1q2024
+docker exec -ti tpe_arqui_1q2024 make clean -C/root/Toolchain
+docker exec -ti tpe_arqui_1q2024 make clean -C/root
+docker stop tpe_arqui_1q2024
--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 # Compila el proyecto a traves del container de Docker
-docker start tpSO3
-docker exec -ti tpSO3 make clean -C/root/Toolchain
-docker exec -ti tpSO3 make clean -C/root
-docker exec -ti tpSO3 make all -C/root/Toolchain
-docker exec -ti tpSO3 make all -C/root
-docker exec -ti tpSO3 chown -R 1000:1000 /root
-docker stop tpSO3
+docker start tpe_arqui_1q2024
+docker exec -ti tpe_arqui_1q2024 make clean -C/root/Toolchain
+docker exec -ti tpe_arqui_1q2024 make clean -C/root
+docker exec -ti tpe_arqui_1q2024 make all -C/root/Toolchain
+docker exec -ti tpe_arqui_1q2024 make all -C/root
+docker exec -ti tpe_arqui_1q2024 chown -R 1000:1000 /root
+docker stop tpe_arqui_1q2024
